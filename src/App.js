9ba1bd941f4a663586852e8875a6d7b7264351a2
Yashash <yashash.pugalia2004@gmail.com>
2022-01-22 17:03:30 +0530

    Update : )

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./style.css";
 import data from "./data.json";
 
-function Main({ pageName, info }) {
-  return (
-    <main>
-      <h1>{pageName}</h1>
-      <div className="tilesCont">
-        {info.map((e) => {
-          return (
-            <div className={e.type}>
-              <span>{e.icon}</span>
-              <div>
-                <p>{e.name}</p>
-                <p className="tile_desc">{e.desc}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </main>
-  );
-}
-
 function App() {
   const [state, setState] = useState("System");
+
+  useEffect(() => {
+    document.querySelectorAll(".navLink").forEach((e) => {
+      e.addEventListener("click", function () {
+        const current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+      });
+    });
+  });
 
   return (
     <>
       <nav>
         <div className="nav_top">
-          <div className="navLink account" onClick={() => setState("Accounts")}>
-            <img src="img/nav/defAccount.webp" width="60" height="60" alt="" />
+          <div className="account" onClick={() => setState("Accounts")}>
+            <img src="img/nav/defAccount.webp" alt="" height={60} width={60} />
             <div>
               <p>Yashash</p>
               <p>Local Account</p>
@@ -41,10 +32,10 @@ function App() {
           <input type="text" className="search" placeholder="Find a setting " name="search" />
         </div>
         <div className="nav_bottom">
-          {data.navList.map((e) => {
+          {Object.keys(data).map((e) => {
             return (
               <div className="navLink" onClick={() => setState(e)}>
-                <img src={`img/nav/${e}.webp`} alt="" width="16" />
+                <img src={`img/nav/${e}.webp`} alt="" height={16} width={16} />
                 {e}
               </div>
             );
@@ -52,19 +43,30 @@ function App() {
         </div>
       </nav>
 
-      {state === "System" && <Main pageName={state} info={data.System} />}
-      {state === "Bluetooth & devices" && <Main pageName={state} info={data["Bluetooth & devices"]} />}
-      {state === "Network & internet" && <Main pageName={state} info={data["Network & internet"]} />}
-      {state === "Personalisation" && <Main pageName={state} info={data.Personalisation} />}
-      {state === "Apps" && <Main pageName={state} info={data.Apps} />}
-      {state === "Accounts" && <Main pageName={state} info={data.Accounts} />}
-      {state === "Time & language" && <Main pageName={state} info={data["Time & language"]} />}
-      {state === "Gaming" && <Main pageName={state} info={data.Gaming} />}
-      {state === "Accessibility" && <Main pageName={state} info={data.Accessibility} />}
-      {state === "Privacy & security" && <Main pageName={state} info={data["Privacy & security"]} />}
-      {state === "Windows Update" && <Main pageName={state} info={data["Windows Update"]} />}
+      {Object.keys(data).map((e) => {
+        return (
+          state === e && (
+            <main>
+              <h1>{e}</h1>
+              <div className="tilesCont">
+                {data[e].map((e) => {
+                  return (
+                    <div className={e.type}>
+                      <span>{e.icon}</span>
+                      <div>
+                        <p>{e.name}</p>
+                        <p className="tile_desc">{e.desc}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </main>
+          )
+        );
+      })}
 
-      <div className="navMenuBtn">
+      <div className="navMenuBtn" onClick={() => document.querySelector("nav").classList.toggle("open")}>
         <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24}>
           <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z" />
         </svg>
--- src/index.js
+++ src/index.js
@@ -8,19 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-const navMenuBtn = document.querySelector(".navMenuBtn");
-const nav = document.querySelector("nav");
-var navLink = nav.getElementsByClassName("navLink");
-navMenuBtn.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
-for (var i = 0; i < navLink.length; i++) {
-  navLink[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-    this.className += " active";
-  });
-}

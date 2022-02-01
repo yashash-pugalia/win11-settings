1cac3792540d4fa0b59be25254982a84cc80bd65
Yashash <yashash.pugalia2004@gmail.com>
2022-02-02 01:51:15 +0530

    Add Contributors + Update to SCSS
    SCSS instead of CSS
    
    few fixes :)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import "./style.css";
+import "./style.scss";
 import data from "./data.json";
 
 function App() {
@@ -9,11 +9,19 @@ function App() {
   useEffect(() => {
     document.querySelectorAll(".navLink").forEach((e) => {
       e.addEventListener("click", function () {
-        const current = document.getElementsByClassName("active");
+        const current = document.querySelectorAll(".active");
         if (current.length > 0) {
-          current[0].className = current[0].className.replace(" active", "");
+          current[0].classList.remove("active");
         }
-        this.className += " active";
+        this.classList.add("active");
+        document.querySelector("nav").classList.remove("open");
+
+        const marker = document.querySelector(".marker");
+        marker.classList.add("active");
+
+        setTimeout(() => {
+          marker.classList.remove("active");
+        }, 150);
       });
     });
   });
@@ -34,24 +42,25 @@ function App() {
         <div className="nav_bottom">
           {Object.keys(data).map((e) => {
             return (
-              <div className="navLink" onClick={() => setState(e)}>
+              <div key={e} className="navLink" onClick={() => setState(e)}>
                 <img src={`img/nav/${e}.webp`} alt="" height={16} width={16} />
                 {e}
               </div>
             );
           })}
+          <div className="marker"></div>
         </div>
       </nav>
 
       {Object.keys(data).map((e) => {
         return (
           state === e && (
-            <main>
+            <main key={e}>
               <h1>{e}</h1>
               <div className="tilesCont">
                 {data[e].map((e) => {
                   return (
-                    <div className={e.type}>
+                    <div key={e.name} className={e.type}>
                       <span>{e.icon}</span>
                       <div>
                         <p>{e.name}</p>
@@ -67,8 +76,8 @@ function App() {
       })}
 
       <div className="navMenuBtn" onClick={() => document.querySelector("nav").classList.toggle("open")}>
-        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24}>
-          <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 48 48" width={24} height={24}>
+          <path d="M5.5 9a1.5 1.5 0 1 0 0 3h37a1.5 1.5 0 1 0 0-3h-37zm0 13.5a1.5 1.5 0 1 0 0 3h37a1.5 1.5 0 1 0 0-3h-37zm0 13.5a1.5 1.5 0 1 0 0 3h37a1.5 1.5 0 1 0 0-3h-37z" />
         </svg>
       </div>
     </>

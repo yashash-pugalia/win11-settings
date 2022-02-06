749d74ea9541093cce2723e840bbed513a87b08f
Cup Of Coffe <90518786+cupofcoffebruh@users.noreply.github.com>
2022-02-06 21:25:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import "./style.scss";
 import data from "./data.json";
 
+
 function App() {
   const [state, setState] = useState("System");
 
@@ -19,6 +20,7 @@ function App() {
         const marker = document.querySelector(".marker");
         marker.classList.add("active");
 
+  
         setTimeout(() => {
           marker.classList.remove("active");
         }, 150);
@@ -43,7 +45,7 @@ function App() {
           {Object.keys(data).map((e) => {
             return (
               <div key={e} className="navLink" onClick={() => setState(e)}>
-                <img src={`img/nav/${e}.webp`} alt="" height={16} width={16} />
+                <img src={`img/nav/${e}.webp`} draggable="false" alt="" height={16} width={16} />
                 {e}
               </div>
             );
@@ -53,6 +55,8 @@ function App() {
       </nav>
 
       {Object.keys(data).map((e) => {
+
+      
         return (
           state === e && (
             <main key={e}>
@@ -60,19 +64,46 @@ function App() {
               <div className="tilesCont">
                 {data[e].map((e) => {
                   return (
+                  
                     <div key={e.name} className={e.type}>
                       <span>{e.icon}</span>
-                      <div>
+                     
+                        <img src ={e.device_img} className="device_img"></img>
+                        <div className="column_device">
+                        <p className="device_type">{e.device_type}</p>
+                        <p className="device_name">{e.device_name}</p>
+                        <p className="device_rename">{e.device_rename}</p>
+                        </div>
+                        <div className={e.heading_type}>
+                        <div className="column">
+                        <img src ={e.microsoft_logo} draggable="false" className="microsoft_img"></img>
+                        <p className="microsoft">{e.microsoft_365}<br></br><span className="column_lower">{e.microsoft_benefits}</span></p>
+                        </div>
+                        <div className="column">
+                        <img src ={e.onedrive_logo} draggable="false" className="microsoft_img"></img>
+                        <p className="microsoft">{e.onedrive_name}<br></br><span className="column_lower">{e.manage}</span></p>
+                        </div>
+                        <div className="column">
+                        <img src ={e.update_img} draggable="false" className="microsoft_img"></img>
+                        <p className="microsoft">{e.update_text}<br></br><span className="column_lower">{e.update_foot} {Math.floor(Math.random() * 10)} hours ago</span></p>
+                        </div>
+                        </div>
+                        <div>
                         <p>{e.name}</p>
                         <p className="tile_desc">{e.desc}</p>
+                        </div>
                       </div>
-                    </div>
+                  
                   );
+       
                 })}
+
               </div>
             </main>
           )
         );
+                     
+      
       })}
 
       <div className="navMenuBtn" onClick={() => document.querySelector("nav").classList.toggle("open")}>
